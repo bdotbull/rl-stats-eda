e046eaad76cbfa14b836763f2a536ab48f21e4e9
bdotbull <itsbrandonbull@gmail.com>
2020-10-26 16:14:16 -0500

    successful 200

--- src/scraper.py
+++ src/scraper.py
@@ -4,13 +4,31 @@ Web Scraper for ballchasing.com using ballchasing API
 """
 
 # Imports
+import requests
+
+# API Base Path
+api_base_url = 'https://ballchasing.com/api/'
 
 # Authorization token for ballchasing.com API
-def get_auth_token():
-    token_file_path = '../creds/ballchasing_credentials.txt'
-    with open(token_file_path, 'r') as tfp:
-        token = tfp.readlines()[0]
-    return token
+token_file_path = '../creds/ballchasing_credentials.txt'
+with open(token_file_path, 'r') as tfp:
+    token = tfp.readlines()[0]
 
 # Data output path
 data_out_path = ''
+
+
+headers = {
+    'Authorization': token
+}
+
+data = {
+    'pro': True
+}
+
+# initial ping
+endpoint = f"{api_base_url}"
+
+if __name__ == '__main__':
+    r = requests.get(endpoint, headers=headers)
+    print(r.status_code)
