647817d5c639ac5e57246df1df6c3815b5c844de
bdotbull <itsbrandonbull@gmail.com>
2020-10-26 14:12:59 -0500

    wrapped authorization. initial commit for notebook

--- src/scraper.py
+++ src/scraper.py
@@ -6,8 +6,11 @@ Web Scraper for ballchasing.com using ballchasing API
 # Imports
 
 # Authorization token for ballchasing.com API
-token_file_path = ''
-token = ''
+def get_auth_token():
+    token_file_path = '../creds/ballchasing_credentials.txt'
+    with open(token_file_path, 'r') as tfp:
+        token = tfp.readlines()[0]
+    return token
 
 # Data output path
 data_out_path = ''
