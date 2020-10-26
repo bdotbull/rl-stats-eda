34e2363289c209fb17055875adc8ea4ceb8f1ac7
bdotbull <itsbrandonbull@gmail.com>
2020-10-26 13:33:50 -0500

    updated .gitignore.  Initial commit for scraper.py

--- src/scraper.py
+++ src/scraper.py
@@ -3,3 +3,11 @@ bdotbull
 Web Scraper for ballchasing.com using ballchasing API
 """
 
+# Imports
+
+# Authorization token for ballchasing.com API
+token_file_path = ''
+token = ''
+
+# Data output path
+data_out_path = ''
